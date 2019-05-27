62b801b65d880620ed66adda2942c1b18c561c22
Felipe <filorca@miuandes.cl>
2019-05-27 17:07:14 -0400

    posts-comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,13 @@
 class Post < ApplicationRecord
-  belongs_to :wall
-  belongs_to :user
-  belongs_to :admin
-  belongs_to :super_admin
+  belongs_to :wall, optional: true
+  belongs_to :user, optional: true
+  belongs_to :admin, optional: true
+  belongs_to :super_admin, optional: true
 	has_many :comments
+  has_one_attached :image
 	has_many :likes
-	validates :title, :presence => true 
-	validates :description, length: {in: 20..10000} 
+  #has_one_attached :image
+	#validates :title, :presence => true
+	#validates :description, length: {in: 20..10000}
+
 end
