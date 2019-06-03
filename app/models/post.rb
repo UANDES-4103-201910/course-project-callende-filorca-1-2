daa849fbcdb8370ef5a034b239ea234f5bc6b35e
Felipe <filorca@miuandes.cl>
2019-06-02 22:58:19 -0400

    like

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comment
   has_many_attached :image
+  has_many :likes, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,8 @@ Rails.application.routes.draw do
   resources :top
   match '/users',   to: 'users#index',   via: 'get'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  resources :posts do
+    resources :likes
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
